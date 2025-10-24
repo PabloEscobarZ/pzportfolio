2469f25f047ed6da593579c2c53492440909445b
Paolo <paolozagarella@gmail.com>
2025-10-25 00:07:16 +0200

    Set basename for BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import './i18n';
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/pzportfolio">
     <StrictMode>
       <App />
     </StrictMode>
